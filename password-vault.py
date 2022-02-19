cda9da2941c8b95e3af5f6e918c3eb67a638431b
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-02-19 11:48:52 +0300

    will  open vault

--- password-vault.py
+++ password-vault.py
@@ -17,3 +17,18 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
+encryptionkey=0
+def openVault():
+    def on_closing():
+        opnvault_btn['state']=NORMAL
+        window2.destroy()
+ 
+    backend=default_backend()
+    salt=b'2444'
+    kdf=PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=backend
+    )
